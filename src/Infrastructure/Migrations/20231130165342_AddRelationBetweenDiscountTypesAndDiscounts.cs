28a84e97ee84aa0bc2a148e0d271a67ff357a8cb
Mamhoud <sragmahmoud4@gmail.com>
2023-11-30 18:58:48 +0200

    feat: ✨ add new migration (AddRelationBetweenDiscountTypesAndDiscounts)

---
+++ src/Infrastructure/Migrations/20231130165342_AddRelationBetweenDiscountTypesAndDiscounts.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRelationBetweenDiscountTypesAndDiscounts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DiscountTypeId",
+                table: "Discounts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Discounts_DiscountTypeId",
+                table: "Discounts",
+                column: "DiscountTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Discounts_DiscountTypes_DiscountTypeId",
+                table: "Discounts",
+                column: "DiscountTypeId",
+                principalTable: "DiscountTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Discounts_DiscountTypes_DiscountTypeId",
+                table: "Discounts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Discounts_DiscountTypeId",
+                table: "Discounts");
+
+            migrationBuilder.DropColumn(
+                name: "DiscountTypeId",
+                table: "Discounts");
+        }
+    }
+}
