c01c064792a070a62fde6d834bf33801fcdefd38
Mamhoud <sragmahmoud4@gmail.com>
2023-11-30 16:55:35 +0200

    feat: ✨ add new migration (AddRelationBetweenAppointmentsAndASPUser)

---
+++ src/Infrastructure/Migrations/20231130145008_AddRelationBetweenAppointmentsAndASPUser.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRelationBetweenAppointmentsAndASPUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DoctorId",
+                table: "Appointments",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_DoctorId",
+                table: "Appointments",
+                column: "DoctorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Appointments_AspNetUsers_DoctorId",
+                table: "Appointments",
+                column: "DoctorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Appointments_AspNetUsers_DoctorId",
+                table: "Appointments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Appointments_DoctorId",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "DoctorId",
+                table: "Appointments");
+        }
+    }
+}
