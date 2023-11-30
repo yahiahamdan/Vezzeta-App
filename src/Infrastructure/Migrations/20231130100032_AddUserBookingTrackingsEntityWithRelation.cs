f696e1f7234be361f6e68fec7be9b86ade5465ce
Mamhoud <sragmahmoud4@gmail.com>
2023-11-30 12:12:54 +0200

    feat: ✨ add new migration (AddUserBookingTrackingWithRelation)

---
+++ src/Infrastructure/Migrations/20231130100032_AddUserBookingTrackingsEntityWithRelation.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserBookingTrackingsEntityWithRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserBookingTrackings",
+                columns: table => new
+                {
+                    PatientId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ApprovedBookingCount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserBookingTrackings", x => x.PatientId);
+                    table.ForeignKey(
+                        name: "FK_UserBookingTrackings_AspNetUsers_PatientId",
+                        column: x => x.PatientId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserBookingTrackings");
+        }
+    }
+}
