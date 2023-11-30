5c75e86c44c75d4f296bdfffc4f8d6e572668e77
Mamhoud <sragmahmoud4@gmail.com>
2023-11-30 18:35:24 +0200

    feat: ✨ add new migration (AddRelationBetweenBookingsAndDiscounts)

---
+++ src/Infrastructure/Migrations/20231130163144_AddRelationBetweenBookingsAndDiscounts.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRelationBetweenBookingsAndDiscounts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DiscountId",
+                table: "Bookings",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_DiscountId",
+                table: "Bookings",
+                column: "DiscountId",
+                unique: true,
+                filter: "[DiscountId] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Discounts_DiscountId",
+                table: "Bookings",
+                column: "DiscountId",
+                principalTable: "Discounts",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Discounts_DiscountId",
+                table: "Bookings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bookings_DiscountId",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "DiscountId",
+                table: "Bookings");
+        }
+    }
+}
