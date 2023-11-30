d771ba57194744785575d26bb0fdcb1a6100b46f
Mamhoud <sragmahmoud4@gmail.com>
2023-11-30 11:07:41 +0200

    feat: ✨ add new migration (AddExaminationPriceEntity)

---
+++ src/Infrastructure/Migrations/20231130090155_AddExaminationPricesEntity.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExaminationPricesEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExaminationPrices",
+                columns: table => new
+                {
+                    DoctorId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Price = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExaminationPrices", x => x.DoctorId);
+                    table.ForeignKey(
+                        name: "FK_ExaminationPrices_AspNetUsers_DoctorId",
+                        column: x => x.DoctorId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExaminationPrices");
+        }
+    }
+}
