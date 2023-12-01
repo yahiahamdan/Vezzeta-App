339bd168ab5633447ed50205201a116b57e4dd25
Mamhoud <sragmahmoud4@gmail.com>
2023-12-01 10:15:24 +0200

    feat: ✨ add migration (AddRelationBetweenASPUserAndSpecialization)

---
+++ src/Infrastructure/Migrations/20231201080653_AddRelationBetweenASPUserAndSpecialization.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRelationBetweenASPUserAndSpecialization : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SpecializationId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_SpecializationId",
+                table: "AspNetUsers",
+                column: "SpecializationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Specializations_SpecializationId",
+                table: "AspNetUsers",
+                column: "SpecializationId",
+                principalTable: "Specializations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Specializations_SpecializationId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_SpecializationId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "SpecializationId",
+                table: "AspNetUsers");
+        }
+    }
+}
