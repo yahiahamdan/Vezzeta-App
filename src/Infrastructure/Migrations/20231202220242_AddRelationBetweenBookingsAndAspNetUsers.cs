4257b909b3f74ed13cbf275ceb6855ed301425d5
Mamhoud <sragmahmoud4@gmail.com>
2023-12-03 00:08:32 +0200

    feat: ✨ add new migration (AddRelationBetweenASPUserAndBookings)

---
+++ src/Infrastructure/Migrations/20231202220242_AddRelationBetweenBookingsAndAspNetUsers.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRelationBetweenBookingsAndAspNetUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PatientId",
+                table: "Bookings",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_PatientId",
+                table: "Bookings",
+                column: "PatientId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_AspNetUsers_PatientId",
+                table: "Bookings",
+                column: "PatientId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_AspNetUsers_PatientId",
+                table: "Bookings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bookings_PatientId",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "PatientId",
+                table: "Bookings");
+        }
+    }
+}
