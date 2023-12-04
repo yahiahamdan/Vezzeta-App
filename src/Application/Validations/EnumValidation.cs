6a4e87de47aeb18410f3fdec00bd2a785deb2d14
Mamhoud <sragmahmoud4@gmail.com>
2023-12-04 16:21:53 +0200

    feat: ✨ add custom validation (EnumValidationAttribute)

---
+++ src/Application/Validations/EnumValidation.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Validations
+{
+    [AttributeUsage(
+        AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter,
+        AllowMultiple = false
+    )]
+    internal class EnumValidationAttribute : ValidationAttribute
+    {
+        private readonly Type enumType;
+
+        public EnumValidationAttribute(Type enumType) => this.enumType = enumType;
+
+        public override bool IsValid(object value)
+        {
+            if (value == null || string.IsNullOrWhiteSpace(value.ToString()))
+                return false;
+
+            string[] enumNames = Enum.GetNames(this.enumType);
+
+            return enumNames.Contains(value.ToString(), StringComparer.OrdinalIgnoreCase);
+        }
+    }
+}
