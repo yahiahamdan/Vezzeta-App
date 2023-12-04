0ccf416ab02b39bef3749e2f1308fa283758fb58
Mamhoud <sragmahmoud4@gmail.com>
2023-12-04 16:15:59 +0200

    feat: ✨ Add LoginDto for authentication data handling

---
+++ src/Application/Dtos/LoginDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Dtos
+{
+    public class LoginDto
+    {
+        [EmailAddress, Required, MinLength(3), MaxLength(64)]
+        public string Email { get; set; }
+
+        [Required, MinLength(8), MaxLength(32), DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
