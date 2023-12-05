2f2f0ad390f447a795736cc765d7b057c09be802
Mamhoud <sragmahmoud4@gmail.com>
2023-12-05 18:24:28 +0200

    feat: ✨ add file service

---
+++ src/Application/Interfaces/Helpers/IFileHelperService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Application.Interfaces.Helpers
+{
+    public interface IFileHelperService
+    {
+        public Task<string[]> UploadFile(IFormFile file);
+        public void DeleteFile(string imagePath);
+    }
+}
---
+++ src/Infrastructure/Helpers/FileHelperService.cs
@@ -0,0 +1,25 @@
+﻿using Application.Interfaces.Helpers;
+using Microsoft.AspNetCore.Http;
+
+namespace Infrastructure.Helpers
+{
+    public class FileHelperService : IFileHelperService
+    {
+        public async Task<string[]> UploadFile(IFormFile file)
+        {
+            string fileName = $"{Guid.NewGuid().ToString()}-{Path.GetExtension(file.FileName)}";
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "../../uploads");
+            string imagePath = Path.Combine(filePath, fileName);
+
+            using var stream = File.Create(imagePath);
+            await file.CopyToAsync(stream);
+
+            return [fileName, filePath];
+        }
+
+        public void DeleteFile(string imagePath)
+        {
+            File.Delete(imagePath);
+        }
+    }
+}
