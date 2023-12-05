dd430e45cc8ea1736b2431b6005f52ea3664b7ec
Mamhoud <sragmahmoud4@gmail.com>
2023-12-05 22:18:43 +0200

    feat: ✨ add JWT helper service

---
+++ src/Application/Interfaces/Helpers/IJwtHelpService.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+
+namespace Application.Interfaces.Helpers
+{
+    public interface IJwtHelpService
+    {
+        public string GenerateToken(string email, string userId, string role);
+        public ClaimsPrincipal DecodeToken(string accessToken);
+    }
+}
---
+++ src/Infrastructure/Helpers/JwtHelperService.cs
@@ -0,0 +1,71 @@
+﻿using Application.Interfaces.Helpers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Infrastructure.Helpers
+{
+    public class JwtHelperService : IJwtHelpService
+    {
+        private SymmetricSecurityKey key;
+        private IConfiguration configuration;
+
+        public JwtHelperService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:key"]));
+        }
+
+        public ClaimsPrincipal DecodeToken(string accessToken)
+        {
+            var handler = new JwtSecurityTokenHandler().ValidateToken(
+                accessToken,
+                new TokenValidationParameters()
+                {
+                    IssuerSigningKey = key,
+                    ValidIssuer = configuration["JWT:Issuer"],
+                    ValidAudience = configuration["JWT:Audience"],
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                },
+                out SecurityToken securityToken
+            );
+
+            return handler;
+        }
+
+        public string GenerateToken(string email, string userId, string role)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, email),
+                new Claim("userId", userId),
+                new Claim("role", role),
+            };
+
+            var signingCredentials = new SigningCredentials(
+                this.key,
+                SecurityAlgorithms.HmacSha256Signature
+            );
+
+            var description = new SecurityTokenDescriptor()
+            {
+                Issuer = configuration["JWT:Issuer"],
+                IssuedAt = DateTime.UtcNow,
+                Expires = DateTime.UtcNow.AddDays(30),
+                Audience = configuration["JWT:Audience"],
+                SigningCredentials = signingCredentials,
+                Subject = new ClaimsIdentity(claims),
+            };
+
+            var handler = new JwtSecurityTokenHandler();
+            var securedToken = handler.CreateToken(description);
+
+            return handler.WriteToken(securedToken);
+        }
+    }
+}
