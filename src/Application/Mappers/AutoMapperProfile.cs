1ae705e3aa48c1d94499e87b080e73b93ba86ac8
Mamhoud <sragmahmoud4@gmail.com>
2023-12-05 18:25:53 +0200

    feat: ✨ setup auto mapper configuration

---
+++ src/Application/Mappers/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using Application.Dtos;
+using AutoMapper;
+using Core.Models;
+
+namespace Application.Mappers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<RegisterDto, ApplicationUser>();
+            CreateMap<ApplicationUser, RegisterDto>();
+        }
+    }
+}
