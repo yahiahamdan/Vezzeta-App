2f70f184a33804a7acb7b2d4c1def9f1e4584579
Mamhoud <sragmahmoud4@gmail.com>
2023-12-07 14:33:28 +0200

    feat: ✨ add DoctorDto api

---
+++ src/Application/Dtos/DoctorDto.cs
@@ -0,0 +1,33 @@
+﻿using Application.Validations;
+using Core.Enums;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.Dtos
+{
+    public class DoctorDto : LoginDto
+    {
+        [Required, MinLength(3), MaxLength(64)]
+        public string FirstName { get; set; }
+
+        [Required, MinLength(3), MaxLength(64)]
+        public string LastName { get; set; }
+
+        [Required, DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public string DateOfBirth { get; set; }
+
+        [Required, RegularExpression(@"^01[0125]\d{8}$")]
+        public string PhoneNumber { get; set; }
+
+        [Required, EnumValidationAttribute(typeof(GendersEnum))]
+        public string Gender { get; set; }
+
+        [Compare(nameof(Password)), DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        [Required, EnumValidationAttribute(typeof(SpecializationsEnum))]
+        public string Specialization { get; set; }
+
+        public IFormFile Image { get; set; }
+    }
+}
