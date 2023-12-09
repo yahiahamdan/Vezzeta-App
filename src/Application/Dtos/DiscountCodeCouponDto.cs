4a82fc3c1d81a672b41400c81e26a99b9790b9a5
Mamhoud <sragmahmoud4@gmail.com>
2023-12-09 23:27:47 +0200

    feat: ✨ add Dto (DiscountCodeCouponDto)

---
+++ src/Application/Dtos/DiscountCodeCouponDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Dtos
+{
+    public class DiscountCodeCouponDto
+    {
+        [MaxLength(8), MinLength(8)]
+        public string? DiscountCodeCoupon { get; set; }
+    }
+}
