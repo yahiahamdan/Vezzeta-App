1fd4ed2b2c94874f5f42e96c6671c08793fc973e
Mamhoud <sragmahmoud4@gmail.com>
2023-12-09 23:32:20 +0200

    feat: ✨ Implement BookingService with corresponding interface

---
+++ src/Application/Interfaces/Services/IBookingService.cs
@@ -0,0 +1,13 @@
+﻿using Application.Dtos;
+
+namespace Application.Interfaces.Services
+{
+    public interface IBookingService
+    {
+        public string CreateNewBooking(
+            string patientId,
+            int appointmentTimeId,
+            DiscountCodeCouponDto discountCodeCouponDto
+        );
+    }
+}
---
+++ src/Infrastructure/Services/BookingService.cs
@@ -0,0 +1,37 @@
+﻿using Application.Dtos;
+using Application.Interfaces.Repositories;
+using Application.Interfaces.Services;
+
+namespace Infrastructure.Services
+{
+    public class BookingService : IBookingService
+    {
+        private readonly IBookingRepository bookingRepository;
+
+        public BookingService(IBookingRepository bookingRepository)
+        {
+            this.bookingRepository = bookingRepository;
+        }
+
+        public string CreateNewBooking(
+            string patientId,
+            int appointmentTimeId,
+            DiscountCodeCouponDto discountCodeCouponDto
+        )
+        {
+            try
+            {
+                var result = bookingRepository.AddNewBooking(
+                    patientId,
+                    appointmentTimeId,
+                    discountCodeCouponDto
+                );
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
